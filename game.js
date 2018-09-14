a3c0f3bc82447b3a5b3582581a312222461810eb
steviekaye <stevie.kaye@gmail.com>
2018-09-15 10:13:17 +1200

    Rename function names, move require to top

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
-const secretNumber = Math.floor(Math.random() * Math.floor(50)) + 1;
-let numGuesses = 0;
 const prompt = require("prompt-async");
 
+const secretNumber = Math.floor(Math.random() * Math.floor(50)) + 1;
+let numGuesses = 0;
+
 console.log("Please guess a number between 1 and 50");
-async function guessAsync() {
+async function guessNumber() {
   if (num_guesses >= 5) {
     console.log(`No more guesses! The answer was ${secretNumber}`);
     return;
@@ -20,17 +21,17 @@ async function guessAsync() {
   } else if (guess > secretNumber) {
     console.log("Too high! Try again");
     numGuesses++;
-    guessAsync();
+    guessNumber();
   } else {
     console.log("Too low! Try again");
     numGuesses++;
-    guessAsync();
+    guessNumber();
   }
 }
 
 async function errorHandlingAsync() {
   try {
-    await guessAsync();
+    await guessNumber();
   } catch (error) {
     console.error("An error occurred: ", error);
   }
